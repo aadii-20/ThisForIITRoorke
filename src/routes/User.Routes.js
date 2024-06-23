add275e58d6a1d597f70ec54fd3021c9cb0adfc3
aadii-20 <aditya.20krl@gmail.com>
2024-06-23 16:28:37 +0530

    Updated

--- src/routes/User.Routes.js
+++ src/routes/User.Routes.js
@@ -1,13 +1,29 @@
 import bodyParser from 'body-parser';
 import express from 'express';
-import {getPublication} from '../controller/User/CodaLabData/index.js'
+
+import { getPublication, getAllStudents, getAllNews, getEvents } from '../controller/User/CodaLabData/index.js'
+
+import { getPublication } from '../controller/User/CodaLabData/index.js'
+import { getHelloWorld } from '../controller/User/SudipRoySirData/SudiPindex.js'
+
 const router = express.Router();
 
 router.use(bodyParser.urlencoded({ extended: false }));
 router.use(bodyParser.json());
- 
+
 //router.get('/SudipSir/publication',SudipSirpublication);
- router.get('/CodaLab/publication',getPublication);
+router.get('/CodaLab/publication', getPublication);
+router.get('/CodaLab/students', getAllStudents);
+router.get('/CodaLab/news', getAllNews);
+router.get('/CodaLab/events', getEvents);
+
+
+
+//This routes from Sudip sir lab
+router.get('/getHelloWorld', getHelloWorld);
+//This routes from coda lab
+router.get('/publication', getPublication);
+
 
 
 
